a4945f1057bae6227ede06768cf56231d9c85d21
manojpatil1987 <manojdpatil365@gmail.com>
2022-02-20 08:26:58 +0530

    changes in search and added new class MailPage

---
+++ src/main/java/Com/qa/pages/MailPage.java
@@ -0,0 +1,12 @@
+package Com.qa.pages;
+
+public class MailPage {
+	
+	public void Email(){
+		
+		System.out.println("Email page");
+	}
+	
+	
+
+}
---
+++ src/main/java/Com/qa/pages/SearchPage.java
@@ -0,0 +1,20 @@
+package Com.qa.pages;
+
+public class SearchPage {
+
+	public void searchPage(){
+		
+		System.out.println("searchpage");
+		
+	}
+	public void searchBinary(){
+		
+		System.out.println("Search Binary");
+		
+		
+	}
+		
+	}
+	
+	
+
